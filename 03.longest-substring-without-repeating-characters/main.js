801352932f446426720c4e20664e7b15e11be1b0
tatwd <leoking9641@hotmail.com>
2018-03-25 17:34:16 +0800

    fix

--- 03.longest-substring-without-repeating-characters/main.js
+++ 03.longest-substring-without-repeating-characters/main.js
@@ -3,15 +3,13 @@
  * @return {number}
  */
 const lengthOfLongestSubstring = function(s) {
-  let obj = {};
-  let len = 0;
-
-  s.split('')
-    .forEach(item => {
-        obj[item] = obj[item] ? obj[item] + 1 : 1;
-      });
-
-  return Object.keys(obj).length;
+  return s
+    .split('')
+    .reduce((arr, item) => {
+      if (!arr.includes(item))
+        arr.push(item);
+      return arr;
+    }, []).length;
 };
 
 let result = lengthOfLongestSubstring("abcabcbb");
