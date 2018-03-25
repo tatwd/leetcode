75803fe29ed4a9064072dc8e400f4f21f28cfed0
tatwd <leoking9641@hotmail.com>
2018-03-25 16:19:04 +0800

    js sln of `add two numbers`

--- 01.two-sum/readme.md
+++ 01.two-sum/readme.md
@@ -1,3 +1,5 @@
+## 01. Two Sum 两数之和
+
 ### Desc
 
 Given an array of integers, return **indices** of the two numbers such that they add up to a specific target.
@@ -6,7 +8,7 @@ You may assume that each input would have **exactly** one solution, and you may 
 
 给定一个整数数列，找出其中和为特定值的那两个数。
 
-你可以假设每个输入都只会有一种答案，同样的元素不能被重用
+你可以假设每个输入都只会有一种答案，同样的元素不能被重用。
 
 ### Example:
 
---
+++ 02.add-two-numbers/main.js
@@ -0,0 +1,57 @@
+/**
+ * Definition for singly-linked list.
+ */
+function ListNode(val) {
+  this.val = val;
+  this.next = null;
+};
+
+ListNode.prototype.nextTo = function(next) {
+  if (next instanceof ListNode)
+    this.next = next;
+  return this;
+};
+
+/**
+* @param {ListNode} l1
+* @param {ListNode} l2
+* @return {ListNode}
+*/
+const addTwoNumbers = function(l1, l2) {
+  let res = new ListNode(0);
+  let temp = res;
+  let [_l1, _l2] = [l1, l2];
+  let sum = 0;
+
+  while (_l1 && _l2) {
+    if (!isNaN(_l1.val)) {
+      sum += _l1.val;
+      _l1 = _l1.next;
+    }
+
+    if (!isNaN(_l2.val)) {
+      sum += _l2.val;
+      _l2 = _l2.next;
+    }
+
+    temp.next = new ListNode(sum % 10);
+    temp = temp.next;
+    sum = (sum / 10) ^ 0;
+  }
+
+  if (sum === 1)
+    temp.next = new ListNode(1);
+
+  return res.next;
+};
+
+// 2 -> 4 -> 3
+let l1 = new ListNode(2).nextTo(
+  new ListNode(4).nextTo(new ListNode(3)));
+
+// 5 -> 6 -> 6
+let l2 = new ListNode(5).nextTo(
+  new ListNode(6).nextTo(new ListNode(6)));
+
+let result = addTwoNumbers(l1, l2);
+console.log(JSON.stringify(result));
