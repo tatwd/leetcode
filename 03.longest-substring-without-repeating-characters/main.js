6cc017686233ef0d84f0c4726372c93687646c24
tatwd <leoking9641@hotmail.com>
2018-03-28 11:40:42 +0800

    fix 02.

--- 03.longest-substring-without-repeating-characters/main.js
+++ 03.longest-substring-without-repeating-characters/main.js
@@ -3,13 +3,34 @@
  * @return {number}
  */
 const lengthOfLongestSubstring = function(s) {
-  return s
-    .split('')
-    .reduce((arr, item) => {
-      if (!arr.includes(item))
-        arr.push(item);
-      return arr;
-    }, []).length;
+  // return s
+  //   .split('')
+  //   .reduce((arr, item) => {
+  //     if (!arr.includes(item))
+  //       arr.push(item);
+  //     return arr;
+  //   }, []).length;
+  // let i, j, len, map = {};
+  // let start = 0, end = 0;
+  // for (i = 0, j = 0, len =  s.length; i < len; ++i) {
+  //   start = j;
+  //   if (map[s[i]])
+  //     j = map[s[i]];
+  //   else
+  //     map[s[i]] = i;
+  // }
+  // end = j;
+  // return s.slice(start, end);
+
+  /*
+   * solution 1 - use Map
+   */
+  let map = new Map();
+  let res = 0;
+
+  // todo
+
+  return res;
 };
 
 let result = lengthOfLongestSubstring("abcabcbb");
