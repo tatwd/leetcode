76e2b19f39403b6f07d6cef76f66f973ebaa057d
tatwd <leoking9641@hotmail.com>
2018-12-26 22:22:59 +0800

    fix to get a conret d

--- 006_zigzag-conversion/main.js
+++ 006_zigzag-conversion/main.js
@@ -5,19 +5,27 @@
  */
 var convert = function(s, numRows) {
   var result = [];
+  var n = (numRows - 1) * 2;
   for (var i = 0; i < numRows; ++i) {
-    // FIXME: get a currect d
-    var d = numRows - i === 1 ? (numRows - 1) * 2 : (numRows - i - 1) * 2;
+    var d = i * 2;
     for (var j = i; j < s.length; j += d) {
-      result.push(s[j]);
+      result += s[j];
+      d = n - d || n;
     }
   }
+  // var curRow = 0;
+  // var goingDown = false;
+  // for (var c of s) {
+  //   result[curRow] = !result[curRow] ? c : result[curRow] + c;
+  //   if (curRow == 0 || curRow == numRows - 1) goingDown = !goingDown;
+  //   curRow += goingDown ? 1 : -1;
+  // }
   return result;
 };
 
 var data = [
-  { s: 'PAYPALISHIRING', numRows: 3 },
-  { s: 'PAYPALISHIRING', numRows: 4 }
+  { s: 'PAYPALISHIRING', numRows: 3 }
+  // { s: 'PAYPALISHIRING', numRows: 4 }
 ];
 for (var i of data) {
   var result = convert(i.s, i.numRows);
