da94c91ab3b9d66f1502ea69f5122d7f0db08319
tatwd <leoking9641@hotmail.com>
2018-12-27 13:49:37 +0800

    003: add solution 2 of js

--- 003_longest-substring-without-repeating-characters/main.js
+++ 003_longest-substring-without-repeating-characters/main.js
@@ -4,15 +4,28 @@
  */
 var lengthOfLongestSubstring = function(s) {
   // Solution 1
-  var chars = [];
+  // var chars = [];
+  // var result = 0;
+  // for (var i = 0, len = s.length; i < len; ++i) {
+  //   var index = chars.indexOf(s[i])
+  //   if (index !== -1) {
+  //     chars = chars.slice(index + 1);
+  //   }
+  //   chars.push(s[i]);
+  //   result = Math.max(result, chars.length);
+  // }
+  // return result;
+
+  // Solution 2
+  // hit_index = max(current_hit_index, hit_index)
+  // result = max(result, index - hit_index)
+  var map = {};
   var result = 0;
+  var hit = -1;
   for (var i = 0, len = s.length; i < len; ++i) {
-    var index = chars.indexOf(s[i])
-    if (index !== -1) {
-      chars = chars.slice(index + 1);
-    }
-    chars.push(s[i]);
-    result = Math.max(result, chars.length);
+    if (map[s[i]] !== undefined) hit = Math.max(map[s[i]], hit);
+    result = Math.max(result, i - hit);
+    map[s[i]] = i;
   }
   return result;
 };
@@ -20,6 +33,7 @@ var lengthOfLongestSubstring = function(s) {
 var data = [
   { s: '', result: 0 },
   { s: ' ', result: 1 },
+  { s: 'abba', result: 2 },
   { s: 'dvdf', result: 3 },
   { s: 'ckilbkd', result: 5 },
   { s: 'abcabcbb', result: 3 },
