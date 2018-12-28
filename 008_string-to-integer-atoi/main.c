53a60a951615e4e067ea66e7585c7a2307c58adf
tatwd <leoking9641@hotmail.com>
2018-12-28 10:11:29 +0800

    008: update c solution

--- 008_string-to-integer-atoi/main.c
+++ 008_string-to-integer-atoi/main.c
@@ -7,37 +7,43 @@ int myAtoi(char *str)
     if (str == NULL || *str == '\0')
         return 0;
 
-    int res = 0, sign = 1, c = 0;
     char *p = str;
+    int res = 0, sign = 1, cur = 0;
+    int i32_max_10r = INT_MAX / 10;
+    int i32_min_10r = INT_MIN / 10;
 
-    /* 排除空格 */
-    for (; isspace(*p); p++)
-        ;
+    /* 排除空白符 */
+    while (isspace(*p))
+        p++;
 
     /* 确定符号 */
-    if (*p == '-') {
-        sign = -1;
-        p++;
-    } else if (*p == '+') {
+    /*
+    if (*p == '-' || *p == '+') {
+        if (*p == '-') sign = -1;
         p++;
     }
+    */
+    ((*p == '-' && (sign = -1)) || *p == '+') && (p++);
 
     for (; *p != '\0' && isdigit(*p); p++) {
-        c = *p - '0';
+        cur = sign * (*p - '0');
 
         /* 溢出检查 */
-        if ((res > INT_MAX / 10) || (res == INT_MAX / 10 && c > 7))
-            return INT_MAX;
-        if ((res < INT_MIN / 10) || (res == INT_MIN / 10 && c > 8))
-            return INT_MIN;
+        if (sign > 0) {
+            if ((res > i32_max_10r) || (res == i32_max_10r && cur > 7))
+                return INT_MAX;
+        } else {
+            if ((res < i32_min_10r) || (res == i32_min_10r && cur < -8))
+                return INT_MIN;
+        }
 
-        res = res * 10 + sign * c;
+        res = res * 10 + cur;
     }
     return res;
 }
 
 /* 测试 */
-int main(int argc, char const *argv[])
+int main(void)
 {
     char *strs[] = {
         "",
@@ -47,10 +53,9 @@ int main(int argc, char const *argv[])
         "4193 with words",
         "words and 987",
         "-91283472332",
-        "-2147483649"
+        "2147483649"
     };
-    for (int i = 0; i < 8; ++i)
-    {
+    for (int i = 0; i < 8; ++i) {
         int result = myAtoi(strs[i]);
         printf("\"%s\" => %d\n", strs[i], result);
     }
