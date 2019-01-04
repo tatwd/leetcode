677d92cbf9c5acdd1c7693afcce55a01858ac6de
tatwd <leoking9641@hotmail.com>
2019-01-05 00:33:54 +0800

    032: fix js solution

--- 032_longest-valid-parentheses/solution.js
+++ 032_longest-valid-parentheses/solution.js
@@ -3,10 +3,10 @@
  * @return {number}
  */
 var longestValidParentheses = function(s) {
-  // TODO
   var buf = [],
-    count = 0;
-  for (var i = 0, len = s.length; i < len; i++) {
+    count = 0,
+    len = s.length;
+  for (var i = 0; i < len; i++) {
     if (buf.length === 0) {
       buf.push(i);
     } else {
@@ -17,13 +17,23 @@ var longestValidParentheses = function(s) {
       }
     }
   }
-  console.log(buf);
-  return count;
+  if (buf.length === len) return 0;
+  if (buf.length === 0) return len;
+  var low = 0,
+    high = len,
+    i = 1;
+  count = buf[0] - low;
+  for (i = 1; i < buf.length; i++) {
+    count = Math.max(count, buf[i] - buf[i - 1] - 1);
+  }
+  return Math.max(count, high - buf[i - 1] - 1);
 };
 
 var data = [
+  { s: '(', result: 0 },
   { s: '(()', result: 2 },
-  { s: '()(())', result: 6 }
+  { s: '()()', result: 4 },
+  { s: ')((()))(', result: 6 }
 ];
 for (var i of data) {
   var result = longestValidParentheses(i.s);
