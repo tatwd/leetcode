46dca86081939e639606afc4cd35984adf143ca2
tatwd <leoking9641@hotmail.com>
2019-01-07 16:12:49 +0800

    028: todo js solution 2

--- 028_implement-strstr/solution.js
+++ 028_implement-strstr/solution.js
@@ -5,15 +5,41 @@
  */
 var strStr = function(haystack, needle) {
   // Solution 1
+  // var ans = -1,
+  //   len1 = haystack.length,
+  //   len2 = needle.length;
+  // for (var i = 0, j = 0; i < len1 && j < len2; i++) {
+  //   if (haystack[i] === needle[j]) {
+  //     j++;
+  //   } else {
+  //     i = i - j;
+  //     j = 0;
+  //   }
+  // }
+  // if (j === len2) return i - j;
+  // return ans;
+
+  // Solution 2
+  // 使用 KMP 算法
   var ans = -1,
     len1 = haystack.length,
     len2 = needle.length;
-  for (var i = 0, j = 0; i < len1 && j < len2; i++) {
-    if (haystack[i] === needle[j]) {
+
+  var get_k = function(s) {
+    var next = [], k = -1;
+    for (var i = 0, len = s.length; i < len - 1; i++) {
+      // TODO
+    }
+    return next;
+  };
+  var next = get_k(needle);
+
+  for (var i = 0, j = 0; i < len1 && j < len2; ) {
+    if (j == -1 || haystack[i] === needle[j]) {
       j++;
+      i++;
     } else {
-      i = i - j;
-      j = 0;
+      j = next[j];
     }
   }
   if (j === len2) return i - j;
